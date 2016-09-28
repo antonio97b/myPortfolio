2db52e458c2a8267a1f3aaaac7b69d1be59c871b
Tony <tony.navarro@gmail.com>
2016-09-28 16:53:37 -0700

    -Remove commented out code.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,32 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  // init: function() {
-    
-  //   // Variable declerations
-  //   var backgrounds = [];
-  //   var body = Ember.$('body');
-  //   var current = 0;
-
-  //   // Preload the images to avoid flicker.
-  //   function imagesPreload(){
-  //     backgrounds = new Array("/images/bg.jpg", "/images/bg2.jpg", "/images/bg3.jpg");
-  //     for (var i=0; i<backgrounds.length; i++) {
-  //      (new Image()).src = backgrounds[i];
-  //     }
-  //     return backgrounds;
-  //   }
-
-  //   imagesPreload();
-
-  //   // Background cycle.
-  //   function nextBackground() {
-  //     body.css('background-image', 'url(' + [backgrounds[current = ++current % backgrounds.length]] + ')');
-
-  //     setTimeout(nextBackground, 5000);
-  //   }
-  //   setTimeout(nextBackground, 5000);
-  //     body.css('background-image', backgrounds[0]);
-  // }
-
 });
